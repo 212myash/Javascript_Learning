f122611f30322b1a0bb7f68725b66fd1c6185492
Yash Raj <212myashraj@gmail.com>
2026-02-27 14:24:34 +0530

    .

--- javascript-basics/variables.js
+++ javascript-basics/variables.js
@@ -6,16 +6,13 @@ This program demonstrates:
 3. Printing output using console.log()
 */
 
-// String type variable (text value)
 let name = "Yash";
 
-// Number type variable
 let age = 20;
 
-// Boolean type variable (true/false)
 let isStudent = true;
 
-// Printing values to console
+
 console.log("Name:", name);
 console.log("Age:", age);
 console.log("Is Student:", isStudent);
